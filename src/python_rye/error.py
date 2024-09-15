98f3a94c455b8e671d6043b2f6e0146b762b96ad
yamap55 <1785817+yamap55@users.noreply.github.com>
2024-09-15 11:19:22 +0900

    Revert "temp: pyright check test"
    This reverts commit 44a9a3bc75809b89a035357f6a538cc707f5700a.

--- src/python_rye/error.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(a: int, b: int) -> int:
-    """Add"""
-    error_point = "error"
-    return a + b
-
-
-# pyright check error
-result = add_numbers(10, "20")
--- src/python_rye/hoge.py
+++ src/python_rye/hoge.py
@@ -14,13 +14,3 @@ class Hoge:
         """return piyo"""
         logger.info("piyo")
         return "piyo"
-
-
-def add_numbers(a: int, b: int) -> int:
-    """Add"""
-    error_point = "error"
-    return a + b
-
-
-# pyright check error
-result = add_numbers(10, "20")
